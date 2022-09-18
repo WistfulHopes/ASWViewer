ec3a7a2b26c5a75c4cfe70e34a9ac6eb2bf4b092
WistfulHopes <kp72002@gmail.com>
2022-09-18 13:14:08 -0700

    fixed crashing on non-existent jonbin load

--- PacHandler.gd
+++ PacHandler.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d28a4ce01b1907e4e8f5c75f544c221d9ea53befe077a9edd208be25fae3c818
-size 7949
+oid sha256:5f573df9b0f07ef0af4920c31ab4c867cc283a7fc09dcd1ca4baea1b295a1e13
+size 8031
